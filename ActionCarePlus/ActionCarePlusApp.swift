803ba1b41827afc159e013ffc7b2b59f285dd509
Donta Delafose <DontaDelafose11092001@gmail.com>
2026-03-01 21:21:22 -0600

    Initial Commit

---
+++ ActionCarePlus/ActionCarePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionCarePlusApp.swift
+//  ActionCarePlus
+//
+//  Created by CAIT on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ActionCarePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionCarePlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActionCarePlus
+//
+//  Created by CAIT on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
